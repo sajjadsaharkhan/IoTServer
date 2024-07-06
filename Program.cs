0312cd1393da7bcdb48f4986272ae082fb9caedf
Sajjad Saharkhan <sahand540@gmail.com>
2024-07-07 00:05:08 +0330

    Add vm

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 using System.Net.Http.Headers;
 using System.Text.Json;
+using Microsoft.AspNetCore.Mvc;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,27 +14,22 @@ app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
-app.MapPost("/api/upload", async (HttpRequest request) =>
+app.MapPost("/api/upload", async (HttpRequest request, [FromBody] UploadInVm inVm) =>
     {
         const string botToken = "6414123044:AAFSEf5tfb7TCVv4__wiPa7EWKnHb0xuOo4";
         const string chatId = "-1002239234726";
 
-        using var reader = new StreamReader(request.Body);
-        var body = await reader.ReadToEndAsync();
-        var jsonDocument = JsonDocument.Parse(body);
-        var base64Image = jsonDocument.RootElement.GetProperty("image").GetString();
-
-        var imageBytes = Convert.FromBase64String(base64Image!);
+        var imageBytes = Convert.FromBase64String(inVm.Image);
         using var content = new MultipartFormDataContent();
         var fileStreamContent = new ByteArrayContent(imageBytes);
         fileStreamContent.Headers.ContentType = new MediaTypeHeaderValue("image/jpeg");
         content.Add(fileStreamContent, "photo", "image.jpg");
 
-        var telegramApiUrl = $"https://api.telegram.org/bot{botToken}/sendPhoto?chat_id={chatId}";
+        const string telegramApiUrl = $"https://api.telegram.org/bot{botToken}/sendPhoto?chat_id={chatId}";
         using var httpClient = new HttpClient();
         var response = await httpClient.PostAsync(telegramApiUrl, content);
 
-        var responseBody = await response.Content.ReadAsStringAsync();
+        await response.Content.ReadAsStringAsync();
 
         return response.IsSuccessStatusCode
             ? Results.Ok("Image sent to Telegram successfully.")
@@ -42,3 +38,6 @@ app.MapPost("/api/upload", async (HttpRequest request) =>
     .WithOpenApi();
 
 app.Run();
+
+
+public record UploadInVm(string Image);
